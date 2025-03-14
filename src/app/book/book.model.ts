ccf8d171ef069928339fbc295cbb8ad9296b103d
Dami <damianev12@gmail.com>
2025-03-14 18:15:48 +0100

    Configure book model and service

---
+++ src/app/book/book.model.ts
@@ -0,0 +1,9 @@
+export type Book = {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  cover: string;
+  publicationYear: number;
+  status: string;
+};
---
+++ src/app/book/book.service.ts
@@ -0,0 +1,64 @@
+import { Book } from './book.model';
+
+export class BookService {
+  books: Book[] = [
+    {
+      id: 1,
+      title: 'Book 1',
+      author: 'Author 1',
+      genre: 'Fiction',
+      cover: 'cover1.jpg',
+      publicationYear: 2001,
+      status: 'Read',
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      author: 'Author 2',
+      genre: 'Non-Fiction',
+      cover: 'cover1.jpg',
+      publicationYear: 2002,
+      status: 'Unread',
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      author: 'Author 3',
+      genre: 'Fantasy',
+      cover: 'cover1.jpg',
+      publicationYear: 2003,
+      status: 'Read',
+    },
+    {
+      id: 4,
+      title: 'Book 4',
+      author: 'Author 4',
+      genre: 'Comedy',
+      cover: 'cover1.jpg',
+      publicationYear: 2004,
+      status: 'Unread',
+    },
+    {
+      id: 5,
+      title: 'Book 5',
+      author: 'Author 5',
+      genre: 'Slice of Life',
+      cover: 'cover1.jpg',
+      publicationYear: 2005,
+      status: 'Read',
+    },
+    {
+      id: 6,
+      title: 'Book 6',
+      author: 'Author 6',
+      genre: 'Drama',
+      cover: 'cover1.jpg',
+      publicationYear: 2006,
+      status: 'Unread',
+    },
+  ];
+
+  getList(): Book[] {
+    return this.books;
+  }
+}
